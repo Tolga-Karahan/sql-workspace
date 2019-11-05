11011cc0e906ac47e99ec707b8ae61a4ec94f443
tkarahan <karahantlg@gmail.com>
2019-11-05 21:48:07 +0300

    grouping and summarizing added

---
+++ grouping-and-summarizing/grouping-and-summarizing.sql
@@ -0,0 +1,7 @@
+# Aggregate fonksiyonları, birden fazla satırda bulunan değerleri; bu değerler üzerinde bir
+# işlem yaparak tek bir değere dönüştürür. Bazı aggregate fonksiyonları aşağıdaki gibidir:
+
+# count() fonksiyonu satır sayısını sayar. count(*) kullanımı NULL değerler de dahil olmak
+# üzere tablodaki tüm satır sayısını verir. count(column_name) kullanımı, argüman olarak
+# verilen sütundaki NULL içermeyen satır sayısını verir.
+
