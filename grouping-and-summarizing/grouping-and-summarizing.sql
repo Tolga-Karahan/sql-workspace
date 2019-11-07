31f4c252fda4f306cfeb80c1422e522d14252e28
tkarahan <karahantlg@gmail.com>
2019-11-07 21:47:49 +0300

    grouping and summarizing v2 added

--- grouping-and-summarizing/grouping-and-summarizing.sql
+++ grouping-and-summarizing/grouping-and-summarizing.sql
@@ -11,13 +11,15 @@ SELECT count(DISTINCT libname);
 # döndürür. Bazen negatif değerler ve ya çok yüksek değerler çeşitli durumları temsil etmek için
 # kullanılabilirler. Bu durumda sütun üzerindeki istatistikler hatalı çıkabilir. Bunu önceden
 # tespit etmek için max() ve min() fonksiyonlarını kullanabiliriz. Tablolar oluşturulurken
-# böyle bir kullanım yerine, negatif ve ya çok büyük değerler için NULL kullanabiliriz. Sebenini
+# böyle bir kullanım yerine, negatif ve ya çok büyük değerler için NULL kullanabiliriz. Sebebini
 # açıklamak içinse flag görevi görecek bir sütun ekleyerek daha doğru bir tasarım yapmış oluruz.
 SELECT max(visits), min(visits)
 FROM pls_fy2014_pupld14a;
 
 # GROUP BY ifadesi ile bir veya birden fazla sütun kullanarak gruplar oluşturabiliriz ve bu 
-# gruplar üzerinde aggregation fonksiyonlarıı kullanabiliriz. 
+# gruplar üzerinde aggregation fonksiyonları kullanabiliriz. SELECT ifadesi ile seçtiğimiz
+# sütunlar ya bir aggregate fonksiyonunun argümanı olmalıdır ya da GROUP BY ifadesi ile birlikte
+# kullanılmalıdır. 
 SELECT stabr, count(*)
 FROM pls_fy2014_pupld14a
 GROUP BY stabr
@@ -29,6 +31,24 @@ FROM pls_fy2014_pupld14a
 GROUP BY stabr, stataddr
 ORDER BY stabr ASC, count(*) DESC;
 
+# 2009 ve 2014 yılında açık olan kütüphanelerdeki ziyaretçi değişimine bakalım.
+SELECT sum(l2009.visits) AS visits_09,
+	   sum(l2014.visits) AS visits_14,
+	   sum(l2009.visits) - sum(l2014.visits) AS difference
+FROM pls_fy2009_pupld09a l2009 JOIN pls_fy2014_pupld14a l2014
+ON l2009.fscskey = l2014.fscskey
+WHERE l2009.visits >= 0 AND l2014.visits >= 0;
+
+# 2009 ve 2014 yılında açık olan kütüphanelerdeki ziyaretçi değişimini eyaletlere göre inceleyelim.
+SELECT l2009.stabr,
+       sum(l2009.visits) AS visits_09,
+       sum(l2014.visits) AS visits_14,
+       round((CAST(sum(l2009.visits) AS numeric (10, 1)) - sum(l2014.visits))
+	   / sum(l2009.visits) * 100, 2) AS percent_change
+FROM pls_fy2009_pupld09a l2009 JOIN pls_fy2014_pupld14a l2014
+ON l2009.fscskey = l2014.fscskey
+GROUP BY l2009.stabr
+ORDER BY percent_change DESC;
 
 
 
