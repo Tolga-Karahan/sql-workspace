f78cf8607fd4044f2f369453a51541995c70df9d
tkarahan <karahantlg@gmail.com>
2019-11-27 22:43:28 +0300

    working with dates and times v2 added

--- working-with-dates-and-times/working-with-dates-and-times.sql
+++ working-with-dates-and-times/working-with-dates-and-times.sql
@@ -41,7 +41,7 @@ SELECT
 SELECT
 	make_date(1994, 9, 23) AS "date",
 	make_time(8, 3, 17.4) AS "time",
-	make_time(1994, 9, 23, 8, 3, 17.4, 'Europe/Istanbul') AS "timestamp"
+	make_timestamptz(1994, 9, 23, 8, 3, 17.4, 'Europe/Istanbul') AS "timestamp"
 
 # Bir sorgu içerisinde güncel tarih ve zamana ihtiyaç duyarsak aşağıdaki SQL fonksiyonlarını kullanabiliriz:
 
@@ -68,3 +68,153 @@ CREATE TABLE current_time_comparison(
 INSERT INTO current_time_comparison (current, clock_current)
 	(SELECT current_timestamp, clock_timestamp()
 		FROM generate_series(1, 1000));
+
+# Zaman bölgeleri, tarih ve zaman verisinin geçerli olduğu bölgeyi yansıtır. SHOW timezone; komutu ile 
+# serverımızın bulunduğu zaman bölgesini öğrenebiliriz. Aşağıdaki sorgu ile zaman bölgelerinin isimlerini,
+# kısaltmalarını ve UTC offsetlerini görebiliriz:
+SELECT * 
+FROM pg_timezone_names;
+
+# WHERE keywordü ile zaman bölgelerini filtreleyebiliriz:
+SELECT * 
+FROM pg_timezone_names
+WHERE name LIKE 'Europe%';
+
+# Serverın default zaman bölgesi ve daha pek çok konfügürasyon parametresi postgresql.conf dosyasında bulunur.
+# SET timezone TO komutu ile konfügürasyon dosyasını değiştirmeksizin, sadece bulunduğumuz oturum içerisinde
+# zaman bölgesini değiştirebiliriz.
+SET timezone TO 'GMT';
+
+# AT TIME ZONE ifadesi ile seçtiğimiz zaman ve tarih bilgisini belirttiğimiz zaman bölgesinde alabiliriz.
+SELECT testtime AT TIME ZONE '+03'
+FROM testtimezone;
+
+# Tarih ve zaman verileri üzerinde de aritmetik işlemler yapabiliriz.
+SELECT '1994/09/23'::date - '1994/09/20'::date;
+
+# date ve interval tipinde verileri toplayabiliriz.
+SELECT '1994/09/23'::date + '25 years'::interval;
+
+# New York taksilerinin verilerini içeren veri kümemizi import edelim.
+CREATE TABLE nyc_yellow_taxi_trips_2016_06_01(
+	trip_id bigserial PRIMARY KEY,
+	vendor_id varchar(1) NOT NULL,
+	tpep_pickup_datetime timestamp with time zone NOT NULL,
+	tpep_dropoff_datetime timestamp with time zone NOT NULL,
+	passenger_count integer NOT NULL,
+	trip_distance numeric(8,2) NOT NULL,
+	pickup_longitude numeric(18,15) NOT NULL,
+	pickup_latitude numeric(18,15) NOT NULL,
+	rate_code_id varchar(2) NOT NULL,
+	store_and_fwd_flag varchar(1) NOT NULL,
+	dropoff_longitude numeric(18,15) NOT NULL,
+	dropoff_latitude numeric(18,15) NOT NULL,
+	payment_type varchar(1) NOT NULL,
+	fare_amount numeric(9,2) NOT NULL,
+	extra numeric(9,2) NOT NULL,
+	mta_tax numeric(5,2) NOT NULL,
+	tip_amount numeric(9,2) NOT NULL,
+	tolls_amount numeric(9,2) NOT NULL,
+	improvement_surcharge numeric(9,2) NOT NULL,
+	total_amount numeric(9,2) NOT NULL
+);
+
+COPY nyc_yellow_taxi_trips_2016_06_01 (
+vendor_id,
+tpep_pickup_datetime,
+tpep_dropoff_datetime,
+passenger_count,
+trip_distance,
+pickup_longitude,
+pickup_latitude,
+rate_code_id,
+store_and_fwd_flag,
+dropoff_longitude,
+dropoff_latitude,
+payment_type,
+fare_amount,
+extra,
+mta_tax,
+tip_amount,
+tolls_amount,
+improvement_surcharge,
+total_amount
+)
+FROM 'path_to_data'
+WITH (FORMAT CSV, HEADER, DELIMITER ',');
+
+CREATE INDEX tpep_pickup_idx
+ON nyc_yellow_taxi_trips_2016_06_01 (tpep_pickup_datetime);
+
+# Müşteri yoğunluklarına göre saatleri sıralayalım.
+SELECT date_part('hour', tpep_pickup_datetime) AS "pickup_hour",
+       sum(passenger_count) AS "customer_sum"
+FROM nyc_yellow_taxi_trips_2016_06_01
+GROUP BY pickup_hour
+ORDER BY customer_sum;
+
+# Taksi seferlerinin frekanslarına göre saatleri sıralayalım.
+SELECT date_part('hour', tpep_pickup_datetime) AS "pickup_hour",
+       count(*) AS freq
+FROM nyc_yellow_taxi_trips_2016_06_01
+GROUP BY pickup_hour
+ORDER BY freq DESC;
+
+# Elde ettiğimiz sonuçları sonradan görselleştirme yapmak için bir dosyaya da yazabiliriz.
+COPY (
+		SELECT date_part('hour', tpep_pickup_datetime) AS "pickup_hour",
+		       count(*) AS freq
+		FROM nyc_yellow_taxi_trips_2016_06_01
+		GROUP BY pickup_hour
+		ORDER BY freq DESC)
+TO 'path_to_file'
+WITH (FORMAT CSV, HEADER);
+
+# Yolculuk sürelerine göre saatleri sıralayalım.
+SELECT date_part('hour', tpep_pickup_datetime) AS "pickup_hour",
+       percentile_cont(0.5) WITHIN GROUP (
+           ORDER BY tpep_dropoff_datetime - tpep_pickup_datetime)
+           AS "median_trip_interval"
+FROM nyc_yellow_taxi_trips_2016_06_01
+GROUP BY pickup_hour
+ORDER BY median_trip_interval DESC;
+
+# Tren servisi tablomuzu oluşturalım. Bu veride ABD'yi boydan boya kat eden ve 4 zaman bölgesi içerisinden
+# geçen bir tren seferi ile ilgili veriler var. 4 tane zaman bölgesi söz konusu olduğu için hesaplamalara
+# dikkat edilmeli.
+CREATE TABLE train_rides(
+	trip_id bigserial PRIMARY KEY,
+	segment varchar(50) NOT NULL,
+	departure timestamp with time zone NOT NULL,
+	arrival timestamp with time zone NOT NULL
+);
+
+INSERT INTO train_rides (segment, departure, arrival)
+VALUES ('Chicago to New York', '2017-11-13 21:30 CST', '2017-11-14 18:23 EST'),
+	   ('New York to New Orleans', '2017-11-15 14:15 EST', '2017-11-16 19:32 CST'),
+	   ('New Orleans to Los Angeles', '2017-11-17 13:45 CST', '2017-11-18 9:00 PST'),
+	   ('Los Angeles to San Francisco', '2017-11-19 10:10 PST', '2017-11-19 21:24 PST'),
+	   ('San Francisco to Denver', '2017-11-20 9:10 PST', '2017-11-21 18:38 MST'),
+	   ('Denver to Chicago', '2017-11-22 19:10 MST', '2017-11-23 14:50 CST');
+
+SET TIME ZONE 'US/Central';
+
+# Her bir segmenti, kalkış saatini ve yolculuk süresini gösterelim.
+SELECT segment,
+       to_char(departure, 'YYYY-MM-DD HH12:MI a.m. TZ') AS departure,
+       arrival - departure AS segment_time
+FROM train_rides;
+
+# Segmentler arası toplam yolculuk süresini bulmak için sum() fonksiyonunu OVER keywordü ile birlikte
+# kullanarak running sum hesaplayabiliriz. Fakat 24 saatten fazla süren yolculuklarda süre gün ve zaman
+# olarak, 24 saatin altında süren yolculuklar için ise sadece zaman olarak verildiği için günler ve zaman
+# kendi arasında toplanır ve istenmeyen bir sonuç ortaya çıkar. Bunun etrafından aşağıdaki syntax ile
+# dolaşabiliriz:
+SELECT segment,
+       arrival - departure AS "segment_time",
+       sum(date_part('epoch', arrival - departure))
+           OVER (ORDER BY trip_id) * interval '1 second' AS "cumulative_time"
+FROM train_rides;
+
+# epoch argümanı ile 1 Ocak 1970 tarihinden itibaren geçen saniyeleri tarihten çıkartıyoruz ve arından 
+# bir interval ile çarparak veri tipini intervala çeviriyoruz.
