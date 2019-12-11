72b54427ea682ff9bc60c725a41616afd3a06c37
tkarahan <karahantlg@gmail.com>
2019-12-11 22:26:35 +0300

    text mining v2 added

--- text-mining/text-mining.sql
+++ text-mining/text-mining.sql
@@ -35,3 +35,76 @@
 # replace(string, from, to) fonksiyonu ile string içerisindeki karakterleri replace edebiliriz.
 # from argümanı ile belirtilen karakterler to argümanı ile belirtilen karakterler ile yer
 # değiştirir.
+
+# Aradığımız stringler belirli formatlara uyuyorsa bu formatları regex ile ifade ederek eşleştirme
+# yapabiliriz. Ardından paternleri WHERE ifadesi ile birlikte kullanarak filtreleme yapabiliriz ve 
+# ya regex fonksiyonlarını kullanarak aynı paternleri taşıyan metinleri çıkartabilir ve üzerinde
+# çalışabiliriz. Regex notasyonunu inceleyelim:
+
+# *, satırbaşını belirten karakter haricinde tüm karakterler ile eşleşir.
+# [FGz], köşeli parantez içerisindeki herhangi bir karakter ile eşleşir. Bu örnekte F, G ve ya z...
+# [a-z], - kullandığımız zaman bir aralık belirtmiş oluruz. Burada tüm küçük harfler ile eşleşir.
+# [^a-z], ^ kullandığımız zaman tümleyenini alırız. Bu örnekte küçük harf olmayan tüm karakterler
+# ile eşleşir. 
+# \w, herhangi bir harf, numara ve ya _ ile eşleşir.
+# \d, herhangi bir dijit ile eşleşir.
+# \s, bir boşluk karakteri ile eşleşir.
+# \t, bir tab karakteri ile eşleşir.
+# \n, yeni satır karakteri ile eşleşir.
+# \r, satırbaşı karakteri ile eşleşir.
+# ^, stringin başından itibaren eşleştirme yap anlamına gelir.
+# $, stringin sonuna kadar eşleştirme yap anlamına gelir.
+# ?, solundaki paterni 0 ve ya 1 kez eşleştir anlamına gelir.
+# +, solundaki paterni 1 ve ya daha fazla sayıda eşleştir anlamına gelir.
+# {m}, solundaki paterni tam olarak m kez eşleştir anlamına gelir.
+# {m, n}, solundaki paterni m-n kez eşleştirir.
+# |, soldaki ve ya sağdaki paterni eşleştirir.
+# (), öncelik belirtmek ve ya belirtilen patern grubunu yakalamak için kullanılır. Sonuç olarak 
+# sadece yakalanan grup döner.
+# (?:), grup oluşturur fakat yakalamaz.
+
+# Basit regex kullanarak çeşitli karakterleri eşleyebiliriz ve kaç tane ve nerede eşleneceklerini
+# belirtebiliriz. substring(string from regex) fonksiyonu ile string içerisinde regex eşleştirebiliriz.
+
+# Üzerinde çalışacağımız veriyi yükleyelim.
+CREATE TABLE crime_reports(
+	crime_id bigserial PRIMARY KEY,
+	date_1 timestamp with time zone,
+	date_2 timestamp with time zone,
+	street varchar(250),
+	city varchar(200),
+	crime_type varchar(100),
+	description text,
+	case_number varchar(50),
+	original_text text NOT NULL
+);
+
+COPY crime_reports
+FROM '/home/tkarahan/Documents/my-repositories/practical-sql/crime_reports.csv'
+WITH (FORMAT CSV, HEADER OFF, QUOTE '"');
+
+# Birden fazla eşleştirme yapabilmek için regexp_match(string, regex) fonksiyonunu kullanabiliriz.
+# Böylece her eşleştirmeden oluşan bir dizi elde edebiliriz. Eğer eşleştirme yoksa geriye NULL
+# döndürür. regexp_match fonksyionu PostgreSQL'e özgü bir fonksiyondur.
+
+# Orijinal metinden tarihleri çıkartalım:
+SELECT crime_id,
+       regexp_match(original_text, '\d{1,2}\/\d{1,2}\/\d{2}')
+FROM crime_reports;
+
+# İki tane tarih bilgisi olan metinlerden her iki tarihi de çıkartalım:
+SELECT crime_id,
+       regexp_matches(original_text, '\d{1,2}\/\d{1,2}\/\d{2}', 'g')
+FROM crime_reports;
+
+# regexp_match fonksiyonu sadece bulduğu ilk eşleşmeyi döndürür. Her bir metinde bulunan tüm eşleşmeleri
+# almak için regexp_matches(string, regexp, flag) fonksiyonunu 'g' flagı ile kullanmalıyız. Böylece ikinci
+# tarih bulunan metinlerden ikinci tarihi de çekmiş oluruz.
+
+# Orijinal metini incelediğimiz zaman tarihlerin '-' ile ayrıldığını görürüz. Dolayısıyla '-' karakteri
+# ile başlayan paternler arayarak ikinci tarihi de bulabiliriz. Fakat sonuçta '-' karakterinin gözükmemesi
+# için grup oluşturarak sadece istediğimiz kısmı yakalamalıyız.
+SELECT crime_id,
+       regexp_match(original_text, '-(\d{1,2}\/\d{1,2}\/\d{2})')
+FROM crime_reports;
+
